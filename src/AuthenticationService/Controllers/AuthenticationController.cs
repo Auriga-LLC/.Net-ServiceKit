d9a5d81f726d6a8c0472bd728f9e7975294962f8
EndarValuk <endarvaluk@gmail.com>
2024-05-21 17:34:23 +0500

    Added some logging

--- src/AuthenticationService/Controllers/AuthenticationController.cs
+++ src/AuthenticationService/Controllers/AuthenticationController.cs
@@ -118,7 +118,10 @@ internal sealed class AuthenticationController
 			return Results.BadRequest(nameof(postLoginUri));
 		}
 
-		OperationContext<OpenIdConnectTokenResponseModel?> tokenExchangeOperation = await provider.ExchangeCodeForTokenAsync(urlProvider.GetTokenIssuerUri(context.Request.Headers, state), code, context.RequestAborted);
+		var issUrl = urlProvider.GetTokenIssuerUri(context.Request.Headers, state);
+		logger.LogWarning(issUrl.AbsoluteUri);
+
+		OperationContext<OpenIdConnectTokenResponseModel?> tokenExchangeOperation = await provider.ExchangeCodeForTokenAsync(issUrl, code, context.RequestAborted);
 		if (tokenExchangeOperation.Result == null)
 		{
 			logger.LogMethodFailedWithErrors(nameof(provider.ExchangeCodeForTokenAsync), tokenExchangeOperation.Errors);
