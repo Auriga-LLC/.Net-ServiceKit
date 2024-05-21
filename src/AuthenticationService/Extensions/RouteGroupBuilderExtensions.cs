ae68d9440e63462c2565325f9bcf6c2469539123
EndarValuk <endarvaluk@gmail.com>
2024-05-21 16:27:38 +0500

    Fixed auth requirements

--- src/AuthenticationService/Extensions/RouteGroupBuilderExtensions.cs
+++ src/AuthenticationService/Extensions/RouteGroupBuilderExtensions.cs
@@ -7,17 +7,22 @@ internal static class RouteGroupBuilderExtensions
 	public static RouteGroupBuilder MapAuthenticationApi(this RouteGroupBuilder routeGroup)
 	{
 		_ = routeGroup.MapGet(RouteConstants.RedirectToLogin, AuthenticationController.RedirectToLogin);
+
 		_ = routeGroup.MapPost(RouteConstants.LoginByCredentials, AuthenticationController.LoginByCredentialsAsync);
-		_ = routeGroup.MapGet(RouteConstants.RequestToken, AuthenticationController.RequestTokenAsync)
-			.RequireAuthorization();
+
+		_ = routeGroup.MapGet(RouteConstants.RequestToken, AuthenticationController.RequestTokenAsync);
+
 		_ = routeGroup.MapGet(RouteConstants.RefreshToken, AuthenticationController.RefreshTokenAsync)
 			.RequireAuthorization();
 		_ = routeGroup.MapPost(RouteConstants.RefreshToken, AuthenticationController.RefreshTokenAsync)
 			.RequireAuthorization();
+
 		_ = routeGroup.MapGet(RouteConstants.Logout, AuthenticationController.LogoutAsync)
 			.RequireAuthorization();
+
 		_ = routeGroup.MapPost(RouteConstants.RevokeTokens, AuthenticationController.RevokeTokensAsync)
 			.RequireAuthorization();
+
 		_ = routeGroup.MapGet(RouteConstants.WhoAmI, AuthenticationController.WhoAmIAsync)
 			.RequireAuthorization();
 
